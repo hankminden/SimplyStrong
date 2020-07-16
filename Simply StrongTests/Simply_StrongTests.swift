95bd4a209826ea40e3aa8de6797cb98f08837909
Henry Minden <henryminden@gmail.com>
2020-07-16 05:41:43 -0700

    Initial Commit

---
+++ Simply StrongTests/Simply_StrongTests.swift
@@ -0,0 +1,34 @@
+//
+//  Simply_StrongTests.swift
+//  Simply StrongTests
+//
+//  Created by Henry Minden on 7/16/20.
+//  Copyright © 2020 ProMeme. All rights reserved.
+//
+
+import XCTest
+@testable import Simply_Strong
+
+class Simply_StrongTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
